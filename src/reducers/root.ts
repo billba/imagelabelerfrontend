09fbe8b2e0851c2dbf3103d460480039ce288a05
Bill Barnes <bill@billba-mbp.guest.corp.microsoft.com>
2017-06-27 15:50:44 -0700

    Another try

--- src/reducers/root.ts
+++ src/reducers/root.ts
@@ -3,13 +3,7 @@ import appsReducer from './AppsReducer';
 import { Epic, combineEpics, createEpicMiddleware, ActionsObservable } from 'redux-observable';
 import { ActionObject } from '../actions/ActionObject';
 import { Observable } from 'rxjs/Observable';
-import 'rxjs/add/operator/do';
-import 'rxjs/add/operator/concat';
-import 'rxjs/add/operator/toArray';
 import 'rxjs/add/operator/mergeMap';
-import 'rxjs/add/operator/map';
-import 'rxjs/add/operator/zip';
-import 'rxjs/add/observable/fromPromise';
 
 export const rootReducer = combineReducers({
     apps: appsReducer,
@@ -18,15 +12,18 @@ export const rootReducer = combineReducers({
 const loadImagesAndLabels : Epic<ActionObject, any> = (action$, store) =>
     action$.ofType('LOAD_IMAGES_AND_LABELS')
     .flatMap(_ =>
-        Observable.fromPromise(fetch('http://getImages').then(response => response.json()))
-        .concat(fetch('http://getLabels').then(response => response.json()))
-        .toArray()
-    )
-    .map(imagesAndLabels => ({
-        type: 'GOT_IMAGES_AND_LABELS',
-        images: imagesAndLabels[0].images,
-        labels: imagesAndLabels[1].labels
-    } as ActionObject));
+        fetch('http://getImages')
+        .then(response => response.json())
+        .then(imagesResponse =>
+            fetch('http://getLabels')
+            .then(response => response.json())
+            .then(labelsResponse => ({
+                type: 'GOT_IMAGES_AND_LABELS',
+                labels: labelsResponse.someFieldName,
+                images: imagesResponse.someFieldName
+            } as ActionObject))
+        )
+    );
 
 export const middleware = applyMiddleware(createEpicMiddleware(combineEpics(
     loadImagesAndLabels
